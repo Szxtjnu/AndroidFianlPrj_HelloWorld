bbe40606cb26cac80f3be1015f4607276a0bee92
AlanMTuring <szxtjnu@gmail.com>
2023-06-06 20:58:40 +0800

    feat(加入Indicator)

--- app/src/main/java/com/andorid/finalprj/menudetailpager/NewsMenuDetailPager.java
+++ app/src/main/java/com/andorid/finalprj/menudetailpager/NewsMenuDetailPager.java
@@ -55,6 +55,7 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
         }
 
         viewPager.setAdapter(new MyNewsMenuDetailPagerAdapter());
+
     }
 
     class MyNewsMenuDetailPagerAdapter extends PagerAdapter {
